8eef059089ebc49dee6800339c562413d250c049
Tobias Baunbaek <freeall@gmail.com>
2020-01-29 10:12:42 +0100

    Better output

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const monitorSocket = require('./socket')
 const minimist = require('minimist')
 const prettyBytes = require('pretty-bytes')
 const log = require('single-line-log').stdout
+const pkgJson = require('./package.json')
 
 const argv = minimist(process.argv.slice(2), { string: 'sim'})
 const token = argv.token
@@ -16,10 +17,15 @@ let capturedPackets = 0
 let capturedBytes = 0
 
 if (!hasAllParams) {
-  console.error('Usage:')
-  console.error('  onomondo-live --token=a1b2c3 --sim=123456789 --filename=output.pcap\n')
-  console.error('You need to use the ID of one of your SIMs, and an API token.')
-  console.error('It is also possible to use the token used in the app. Get this by visiting app.onomondo.com and look at the network tab in developer tools.')
+  console.error([
+    'Usage:',
+    'onomondo-live --token=a1b2c3 --sim=123456789 --filename=output.pcap',
+    '',
+    'You need to use the ID of one of your SIMs, and an API token.',
+    'It is also possible to use the token used in the app. Get this by visiting https://app.onomondo.com and look at the network tab in developer tools.',
+    '',
+    `Version: ${pkgJson.version}`
+  ].join('\n'))
   process.exit(1)
 }
 
@@ -28,26 +34,36 @@ pcapGenerator.writeHeader({ filename })
 connect()
 
 function connect () {
-  const socket = monitorSocket(`${apiUrl}/monitor`)
+  const socket = monitorSocket(`${apiUrl}/monitor?from=onomondo-live`)
 
   socket.on('open', () => {
-    console.log('[Live Monitoring] Connection established')
+    console.log('Connected')
     socket.send('authenticate', token)
   })
 
-  socket.on('error', err => console.error(`[Live Monitoring] Error: ${err}`))
+  socket.on('error', err => {
+    const isNotAuthenticated = err === 'Not authenticated'
+
+    if (isNotAuthenticated) {
+      console.error('Authenticated failed. Token is incorrect')
+      process.exit(1)
+    }
+
+    console.error(`Error: ${err}`)
+  })
+
   socket.on('close', () => {
-    console.log('[Live Monitoring] Connection closed. Trying to re-establish')
+    console.log('Connection closed. Trying to re-establish')
     socket.kill()
     setTimeout(connect, 1000)
   })
 
   socket.on('authenticated', () => {
-    console.log('[Live Monitoring] Authenticated')
+    console.log('Authenticated')
     socket.send('attach', simId)
   })
   socket.on('attached', ({ id, ip }) => {
-    console.log(`[Live Monitoring] Attached. SIM id=${id}. ip=${ip}`)
+    console.log(`Attached. SIM id=${id}. ip=${ip}`)
   })
 
   socket.on('packet', hexString => {
